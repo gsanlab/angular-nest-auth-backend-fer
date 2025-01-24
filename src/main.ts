44a7541b94a3574dd0a4b8f860c4d90547dfa1c3
gsanlab <gsanlab@gmail.com>
2025-01-24 22:51:41 +0000

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(3000);
+  console.log(`App running on port ${3000}`);
+}
+bootstrap();
